571dc12bf4f37b72f2854652dc939a56ed3c6c49
D.Ivanushin <D.Ivanushin@vitebsk.energo.net>
2023-03-12 11:50:59 +0300

    Реализован поиск по задачам

--- src/core/components/smart/milestone/Milestone.tsx
+++ src/core/components/smart/milestone/Milestone.tsx
@@ -125,16 +125,44 @@ function MilestoneBody({
 }: {
   issues: MilestoneBodyProps;
 }) {
+  const [issuesState, setIssuesState] = useState<MilestoneBodyProps>({
+    closedIssues,
+    openIssues,
+    testsIssues,
+  });
+
+  function onChangeSearch(value: string) {
+    function filterIssues(issue: Issue) {
+      return (
+        String(issue.iid).includes(value) ||
+        issue.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+      );
+    }
+
+    if (!value.length) {
+      return setIssuesState({ closedIssues, openIssues, testsIssues });
+    }
+
+    return setIssuesState({
+      closedIssues: closedIssues.filter(filterIssues),
+      openIssues: openIssues.filter(filterIssues),
+      testsIssues: testsIssues.filter(filterIssues),
+    });
+  }
+
   return (
     <div className='milestone-body'>
       <div className='controls'>
         <Button type='primary'>Создать</Button>
-        <Input placeholder='Поиск'></Input>
+        <Input
+          onChange={(event) => onChangeSearch(event.currentTarget.value)}
+          placeholder='Поиск'
+        ></Input>
       </div>
       <Boards>
-        <Board title='Открытые' issues={openIssues} />
-        <Board title='Тестирование' type={'debug'} issues={testsIssues} />
-        <Board title='Выполненные' type={'success'} issues={closedIssues} />
+        <Board title='Открытые' issues={issuesState.openIssues} />
+        <Board title='Тестирование' type={'debug'} issues={issuesState.testsIssues} />
+        <Board title='Выполненные' type={'success'} issues={issuesState.closedIssues} />
       </Boards>
     </div>
   );
